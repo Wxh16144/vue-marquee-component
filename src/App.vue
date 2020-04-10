dfea3f95613293e5ed19c0241ec2763c270861eb
Wuxh <wxh16144@qq.com>
2020-04-10 21:10:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  #app
+    h1.text-center.mt-3 Welcome use template
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+}
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
+
